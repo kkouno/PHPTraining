6b45a58a2368366cce74badbda3fe59553687f86
kkouno <k.kouno2017@gmail.com>
2017-05-09 17:37:39 +0900

    20170509

---
+++ PHPTraining/20170509/test.php
@@ -0,0 +1,11 @@
+<?php
+for($i=0;$i<5;$i++){
+    print "number :".($i+1)."<br>";
+}
+
+$colors=["赤","緑","青","黄色"];
+
+for($i=0;$i<count($colors);$i++){
+    print $colors[$i]."<br>";
+}
+?>
--- PHPTraining/20170509/test4.php
+++ PHPTraining/20170509/test4.php
@@ -1,4 +1,5 @@
 <?php
+/*
     const HP="Hello PHP ";
     const ver=5.3;
 
@@ -42,7 +43,7 @@
     print("<br>");
 
     for($a=0;$a<=3;$a++){
-        print("{$a}<br>");
+        print("$a<br>");
     }
     print("<br>");
     $color=array("Red","Green","Blue");
@@ -54,5 +55,132 @@
         print("{$iro}:{$value}<br>");
     }
     print("<br>");
+
+    for($i=0;$i<10;$i++){
+        if($i==5){
+            echo "5 continue<br>";
+            continue;
+        }
+        echo $i."<br>";
+    }
+    echo "<hr>";
+
     print("<br>");
+
+    for($i=0;$i<10;$i++){
+        if($i==5){
+            echo "break<br>";
+            break;
+        }
+        echo $i . "<br>";
+    }
+
+    print("<br>");
+
+    $level1="B";
+
+    switch($level1){
+        case "A":
+            print "aaa";
+            break;
+        case "B":
+            print "BBB";
+            break;
+        case "C":
+            print "CCC";
+            break;
+        default:
+            print "default";
+            break;
+    }
+
+    print("<br>");
+
+    $color="赤";
+
+    switch($color){
+        case "赤":
+            print "Red";
+            break;
+        case "青":
+            print "Blue";
+            break;
+        default:
+            print "other";
+            break;
+    }
+    print("<br>");
+
+    $score=90;
+    if($score>=80){
+        print "hoge";
+    }else{
+        print "piyo";
+    }
+
+    print("<br>");
+
+    $name=["masuda","ryo","hamana"];
+    print $name[0];
+    $name[3]="hyomori";
+    print $name[3];
+    print("<br>");
+    $a=["C言語","MySQL","PHP","VPS"];
+    print_r($a);
+    echo $a[2];
+    $c[]="a";
+    $c[]="b";
+    $c[]="c";
+    print_r($c);
+
+
+
+    $d[0] = 1;
+    $d[1] = 2;
+    $d[2] = 3;
+    $d[3] = null;
+    $d[4] = 4;
+    $d[6] = 5;
+    $d[] = 6;
+    print_r($d);
+
+    print("<br>");
+    $like2=array("color"=>"red","food"=>"sushi");
+    print_r($like2);
+
+    $like1["color"]="red";
+    $like2["food"]="sushi";
+
+    print_r($like2);
+    print("<br/>");
+    $age=array("yamada"=>25,"honda"=>37,"kato"=>67);
+    foreach($age as $x=>$x_value){
+        echo "Key=".$x." Value=".$x_value;
+        echo "<br>";
+    }
+
+    print("<br>");
+
+    $test=["h","g","f","e","d"];
+    foreach($test as $x=>$value){
+        echo $x.$value;
+    }
+
+    print("<br>");
+    print("<br>");
+    print("<br>");
+    print("<br>");
+    print("<br>");
+*/
+
+for($i=0;$i<5;$i++){
+    print "number :".($i+1)."<br>";
+}
+
+$colors=array("赤","緑","青","黄色");
+
+for($i=0;$i<count($colors);$i++){
+    print $colors[$i]."<br>";
+}
+
 ?>
